8ab3355bcc79ab4549f135f160118c2ee3d2d198
Daeram Chung <daeram.chung@gmail.com>
2021-09-26 21:30:23 +0900

    Dice toggle lock

--- src/Die.js
+++ src/Die.js
@@ -2,17 +2,26 @@ import React, { Component } from "react";
 import "./Die.css";
 
 class Die extends Component {
-  render() {
-    return (
-      <button
-        className={"Die"}
-        style={{ backgroundColor: this.props.locked ? "grey" : "black" }}
-        onClick={this.props.handleClick}
-      >
-        {this.props.val}
-      </button>
-    );
-  }
+	constructor(props) {
+		super(props);
+
+		this.handleToggleLocked = this.handleToggleLocked.bind(this);
+	}
+
+	handleToggleLocked() {
+		this.props.toggleLocked(this.props.idx);
+	}
+	render() {
+		return (
+			<button
+				className={"Die"}
+				style={{ backgroundColor: this.props.locked ? "grey" : "black" }}
+				onClick={this.handleToggleLocked}
+			>
+				{this.props.val}
+			</button>
+		);
+	}
 }
 
 export default Die;
